02133d1648bab2c5dd4fde05a1498f251deccff8
Tim van Dijen <tvdijen@gmail.com>
2021-01-25 20:40:24 +0100

    Modernize hooks

--- hooks/hook_frontpage.php
+++ hooks/hook_frontpage.php
@@ -1,16 +1,20 @@
 <?php
+
+namespace SimpleSAML\Module\saml2debug;
+
+use SimpleSAML\Module;
+
 /**
  * Hook to add the simple consenet admin module to the frontpage.
  *
  * @param array &$links  The links on the frontpage, split into sections.
  */
-function saml2debug_hook_frontpage(&$links) {
-	assert('is_array($links)');
-	assert('array_key_exists("links", $links)');
+function saml2debug_hook_frontpage(array &$links) {
+    assert(is_array($links));
+    assert(array_key_exists("links", $links));
 
-	$links['federation'][] = array(
-		'href' => SimpleSAML_Module::getModuleURL('saml2debug/debug.php'),
-		'text' => array('en' => 'SAML 2.0 Debugger'),
-	);
-	
+    $links['config']['saml2debug'] = [
+        'href' => Module::getModuleURL('saml2debug/debug.php'),
+        'text' => ['en' => 'SAML 2.0 Debugger'],
+    ];
 }
