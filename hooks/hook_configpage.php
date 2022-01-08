f48ef1d19205a46761e126875224e305116a1d94
Tim van Dijen <tvdijen@gmail.com>
2022-01-08 20:40:57 +0100

    Fix hooks

---
+++ hooks/hook_configpage.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+use SimpleSAML\Locale\Translate;
+use SimpleSAML\Module;
+use SimpleSAML\XHTML\Template;
+
+/**
+ * Hook to add the saml2debug module to the config page.
+ *
+ * @param \SimpleSAML\XHTML\Template &$template The template that we should alter in this hook.
+ */
+function saml2debug_hook_configpage(Template &$template): void
+{
+    $template->data['links'][] = [
+        'href' => Module::getModuleURL('saml2debug/debug.php'),
+        'text' => Translate::noop('SAML 2.0 Debugger'),
+    ];
+
+    $template->getLocalization()->addModuleDomain('saml2debug');
+}
